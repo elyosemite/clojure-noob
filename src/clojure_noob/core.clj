852e2857cd3a65fbb49abdac363359101d8cc9e7
Yuri Melo <yurifullstack@gmail.com>
2025-12-05 21:48:59 -0300

    Add conditional logging to core.clj

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,6 +1,13 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(if true
+  (do (println "This is true")
+      "By aquaman")
+  (do (println "This is false")
+      "By batman")
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
