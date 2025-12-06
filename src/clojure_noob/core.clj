67aa6a1ea0b5711d3f76100496fec680c4ab9acb
Yuri Melo <yurifullstack@gmail.com>
2025-12-05 21:52:25 -0300

    Refactor conditional statements and add a personal message in core.clj

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,8 +5,11 @@
   (do (println "This is true")
       "By aquaman")
   (do (println "This is false")
-      "By batman")
-)
+      "By batman"))
+
+(when true
+  (println "Thamirys is my pretty wife!")
+  "She is software engineer")
 
 (defn -main
   "I don't do a whole lot ... yet."
