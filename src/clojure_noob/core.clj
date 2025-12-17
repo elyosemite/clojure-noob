8744b9087f4c41772403326e3d24d2b21e58a057
Yuri Melo <yurifullstack@gmail.com>
2025-12-17 12:14:57 -0300

    Refactor code formatting for consistency and improve error-message function in core.clj

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,43 +1,40 @@
 (ns clojure-noob.core
-   (:gen-class))
+  (:gen-class))
 
- (if true
-   (println "Maria is Jesus's mother")
-   (println "Maria is not Jesus's mother??"))
+(if true
+  (println "Maria is Jesus's mother")
+  (println "Maria is not Jesus's mother??"))
 
- (if false
-   (println "Fatima is arabic name")
-   (println "Fatima is not arabic name"))
+(if false
+  (println "Fatima is arabic name")
+  (println "Fatima is not arabic name"))
 
- (if true
-   (do (println "Clojure is fun")
-       (println "Another text")
-       (println "By Stackoverflow survey"))
-   (do (println "This is false")
-       (println "By batman")))
+(if true
+  (do (println "Clojure is fun")
+      (println "Another text")
+      (println "By Stackoverflow survey"))
+  (do (println "This is false")
+      (println "By batman")))
 
- (and :feeling_super_cool nil false)
+(and :feeling_super_cool nil false)
+
+(def severity :mild)
 
 ; Functions
- (defn error-message
-   [severity]
-   (str "Oh GOD! IT IS A DESASTER! WE ARE "
-        (if (= severity :mild)
-          "MILDY INCONVECIENCED!"
-          "DOOOOOMED")))
+(defn error-message
+  [severity]
+  (str "Oh GOD! IT IS A DESASTER! WE ARE "
+       (if (= severity :mild)
+         "MILDY INCONVECIENCED!"
+         "DOOOOOMED")))
 
- (when true
-   (println "Thamirys is my pretty wife!")
-   "She is software engineer")
-
- (def severity :mild)
- (def error-message "OH MY GOSH! IT'S A DISASTER! WE ARE ")
- (if (= severity :mild)
-   (def error-message (str error-message "MILDLY INCONVENIENCED!"))
-   (def error-message (str error-message "DOOOOOMED!")))
+(when true
+  (println "Thamirys is my pretty wife!")
+  "She is software engineer")
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
+  (error-message severity)
   (println error-message :mild))
